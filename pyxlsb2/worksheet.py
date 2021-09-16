ff2acdab5f983bd6d7d9f6bc5169afae146cc865
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2021-09-16 08:06:45 -0700

    Fix bug when rec.v is None for CELL_ISST

--- pyxlsb2/worksheet.py
+++ pyxlsb2/worksheet.py
@@ -83,7 +83,8 @@ class Worksheet(object):
                 row_num = rec.r
                 row = Row(self, row_num)
             elif rectype == rt.CELL_ISST:
-                row._add_cell(rec.c, self.workbook.get_shared_string(rec.v), rec.f, rec.style)
+                row._add_cell(rec.c, self.workbook.get_shared_string(rec.v) if rec.v else None,
+                              rec.f, rec.style)
             elif rectype >= rt.CELL_BLANK and rectype <= rt.FMLA_ERROR:
                 row._add_cell(rec.c, rec.v, rec.f, rec.style)
             elif rectype == rt.END_SHEET_DATA:
