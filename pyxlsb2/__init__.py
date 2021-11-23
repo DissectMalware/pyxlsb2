0b82ba8170754ba8e56c8225cf586bab59f65487
Malwrologist <9646319+DissectMalware@users.noreply.github.com>
2021-11-23 11:40:36 -0800

    UPDATE - bumping pyxlsb2 version to 0.0.5

--- pyxlsb2/__init__.py
+++ pyxlsb2/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from .workbook import Workbook
 from .xlsbpackage import XlsbPackage
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 
 def open_workbook(name, *args, **kwargs):
