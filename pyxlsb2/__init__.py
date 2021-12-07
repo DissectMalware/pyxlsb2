32d1d4bb640d6b7342a022a46deb863e560c84ae
Malwrologist <9646319+DissectMalware@users.noreply.github.com>
2021-12-07 20:33:20 +0000

    Fixed - added new fstab functions (>0x017B) #13
    https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-xlsb/90a52fcb-ce63-497f-a3d3-173c42d82242

--- pyxlsb2/__init__.py
+++ pyxlsb2/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from .workbook import Workbook
 from .xlsbpackage import XlsbPackage
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 
 def open_workbook(name, *args, **kwargs):
