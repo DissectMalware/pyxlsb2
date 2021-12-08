e109e1407c9b14791e975e43927b581502d77512
Malwrologist <9646319+DissectMalware@users.noreply.github.com>
2021-12-08 00:44:27 +0000

    FIX - handling shared string CELL_ISST (BrtCellIsst record) #15

--- pyxlsb2/worksheet.py
+++ pyxlsb2/worksheet.py
@@ -84,7 +84,11 @@ class Worksheet(object):
                 row = Row(self, row_num)
             elif rectype == rt.CELL_ISST:
                 if rec.v is not None and rec.c is not None:
-                    row._add_cell(rec.c, rec.v, rec.f, rec.style)
+                    if 0 <= rec.v < len(self.workbook.stringtable._strings):
+                        value = self.workbook.get_shared_string(rec.v)
+                    else:
+                        value = rec.v
+                    row._add_cell(rec.c, value, rec.f, rec.style)
 
             elif rectype >= rt.CELL_BLANK and rectype <= rt.FMLA_ERROR:
                 if rec.v is not None and rec.c is not None:
